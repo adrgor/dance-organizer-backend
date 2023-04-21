da9aa69c31e84e13afeb31b7371e4c51faf17177
SG0312389 <adrian.gorski@sabre.com>
2023-04-21 11:58:36 +0200

    Fixes

--- event/src/main/java/org/example/event/exception/EventNotFoundException.java
+++ event/src/main/java/org/example/event/exception/EventNotFoundException.java
@@ -4,6 +4,6 @@ import lombok.Getter;
 
 @Getter
 public class EventNotFoundException extends Exception {
-    private static final int ERROR_CODE = ErrorCodes.EVENT_NOT_FOUND_CODE;
-    private static final String ERROR_MESSAGE = ErrorMessages.EVENT_NOT_FOUND;
+    private final int errorCode = ErrorCodes.EVENT_NOT_FOUND_CODE;
+    private final String errorMessage = ErrorMessages.EVENT_NOT_FOUND;
 }
